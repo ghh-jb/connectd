b08f8299d0c98dd464bf1b45ec56cb077cd932b2
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-04-20 12:46:25 +0300

    update install path, nw it is stored in /usr/bin

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Run as root for successful installation
 gcc -O2 -o ./connectd ./connectd.c -lcrypto
-mv ./connectd /usr/local/bin
-chown root:root /usr/local/bin/connectd
+mv ./connectd /usr/bin
+chown root:root /usr/bin/connectd
 cp ./connectd.service /etc/systemd/system/connectd.service
 systemctl enable connectd
 systemctl start connectd
