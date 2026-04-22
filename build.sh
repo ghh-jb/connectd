5b4fb5de6ca5974dfc07fca3a65ac0d75b79ffac
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-04-22 13:18:54 +0300

    Now it enables and installs to autorun

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ gcc -O2 -o ./connectd ./connectd.c -lcrypto
 mv ./connectd /usr/bin
 chown root:root /usr/bin/connectd
 cp ./connectd.service /etc/systemd/system/connectd.service
-systemctl enable connectd
 systemctl start connectd
+systemctl enable connectd
